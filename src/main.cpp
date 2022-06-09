65296db3c75ace0bc1a95b4a02a3ccfc5670f6cf
Antonius kristian <antoniuskristian072@gmail.com>
2022-06-09 22:10:34 +0700

    sensor data type to float

--- src/main.cpp
+++ src/main.cpp
@@ -52,8 +52,9 @@ void loop()
 
     Serial.print("Nodemcu: ");
     Serial.print(data);
-    // Serial.print(command);
-    // Serial.print(dataValue);
+    Serial.print(command);
+    Serial.print("=>");
+    Serial.print(dataValue);
 
     if(command == "changeCredential"){ 
       // Change credential command format
@@ -74,11 +75,11 @@ void loop()
       // sensorData=[value]
       dataIn = dataValue;
 
-      int sensorValue[5];
+      float sensorValue[5];
 
       for(int i = 0; i < 5; i ++){
         int separator = dataIn.indexOf(',');
-        sensorValue[i] = (int)dataIn.substring(0, separator).toInt();
+        sensorValue[i] = (float)dataIn.substring(0, separator).toFloat();
         dataIn = dataIn.substring(separator+1, dataIn.length());
       }
       
@@ -87,6 +88,9 @@ void loop()
       jsonData["KadarGaram"] = String(sensorValue[0]);
       jsonData["SuhuKotak"] = String(sensorValue[1]);
       jsonData["KetinggianAir"] = String(sensorValue[2]);
+      // batas sensor 80cm kosong
+      // batas sensor 
+
       jsonData["SuhuRuangan"] = String(sensorValue[3]);
       jsonData["Arus"] = String(sensorValue[4]);
 
@@ -106,65 +110,5 @@ void loop()
   {
     Serial.read();
   }
-  
-
-  // if(Serial.available()>0)
-  // {
-  //   // Serial.println("called");
-  //   // String data = Serial.readStringUntil('\n');
-  //   // String command = data.substring(0,data.indexOf('='));
-  //   // String dataValue = data.substring(data.indexOf('[')+1,data.indexOf(']'));
     
-  //   // Serial.println(data);
-  //   // // Serial.println(dataValue);
-  
-  //   // if(command == "changeCredential"){ 
-  //   //   // Change credential command format
-  //   //   // changeCredential=[newCredential]
-  //   //   Credential->writeCredential(dataValue);
-  //   //   cred = Credential->readCredential();
-  //   //   Serial.println(cred);
-  //   // }
-  //   // else if (command == "checkCredential"){
-  //   //   // check credential command format
-  //   //   // checkCredential=[]
-  //   //   cred = Credential->readCredential();
-  //   //   Serial.println(cred);
-  //   // }
-  //   // else if(command == "sensorData"){
-  //   //   // Parse sensor value here
-  //   //   dataIn = dataValue;
-
-  //   //   int sensorValue[5];
-
-  //   //   for(int i = 0; i < 5; i ++){
-  //   //     int separator = dataIn.indexOf(',');
-  //   //     sensorValue[i] = (int)dataIn.substring(0, separator).toInt();
-  //   //     dataIn = dataIn.substring(separator+1, dataIn.length());
-  //   //   }
-      
-  //   //   StaticJsonDocument<256> jsonDoc;
-  //   //   StaticJsonDocument<256> jsonData;
-  //   //   jsonData["KadarGaram"] = String(sensorValue[0]);
-  //   //   jsonData["SuhuKotak"] = String(sensorValue[1]);
-  //   //   jsonData["KetinggianAir"] = String(sensorValue[2]);
-  //   //   jsonData["SuhuRuangan"] = String(sensorValue[3]);
-  //   //   jsonData["Arus"] = String(sensorValue[4]);
-
-  //   //   jsonDoc["credential"] = cred;
-  //   //   jsonDoc["data"] = jsonData;
-
-  //   //   String serial;
-  //   //   serializeJson(jsonDoc, serial);
-  //   //   // Serial.println(serial);
-
-  //   //   String uri = "http://safe-taiga-38670.herokuapp.com/api/sensor/insert";
-  //   //   getapi->PostAPI(uri, serial);
-
-  //   // }
-  //   // else{
-  //   //   Serial.println(data);
-  //   // }
-  // }
-  
 }
